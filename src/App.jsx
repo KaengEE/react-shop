73dd3db51c1527c2a14269fa0ef5b4136300eb87
KaengEE <yopuiqwer@naver.com>
2024-01-02 14:26:54 +0900

    파일정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <>앱</>;
+  return <div className="text-primary">헬로우 월드!</div>;
 }
 
 export default App;
