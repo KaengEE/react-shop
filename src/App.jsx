d609902e208dbde4a3becb030ce4dd1c3064eb49
KaengEE <yopuiqwer@naver.com>
2024-01-02 15:01:43 +0900

    라우팅 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import NotFound from "./pages/not-found/NotFound";
+import UnAuthorized from "./pages/unauthorized/UnAuthorized";
+import Admin from "./pages/admin/Admin";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="/401" element={<UnAuthorized />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
