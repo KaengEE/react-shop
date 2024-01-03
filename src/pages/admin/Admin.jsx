9d7f27cd5d55bfb854adaa414f597dc482dcef9f
KaengEE <yopuiqwer@naver.com>
2024-01-03 14:59:02 +0900

    수정 모달

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import productService from "../../services/product.service";
 import ProductSave from "../../components/ProductSave";
+import Product from "../../models/Product";
 
 const Admin = () => {
   const [productList, setProductList] = useState([]);
   const saveComponent = useRef();
+  //선택한 제품
+  const [selectedProduct, setSelectedProduct] = useState(
+    new Product("", "", 0)
+  );
 
-  //버튼 클릭시 모달창 열기
+  //버튼 클릭시 추가 모달창 열기
   const createProductRequest = () => {
     //ProductSave 모달창의 showProductModal 함수 실행
     saveComponent.current?.showProductModal();
@@ -16,6 +21,13 @@ const Admin = () => {
   const saveProductWatcher = (p) => {
     const newList = productList.concat(p);
     setProductList(newList); //제품리스트 업데이트
+  };
+
+  //수정 모달창
+  const editProductRequest = (item) => {
+    //console.log(item);
+    setSelectedProduct(item);
+    saveComponent.current?.showProductModal();
   };
 
   useEffect(() => {
@@ -62,7 +74,12 @@ const Admin = () => {
                   <td>{item.price} 원</td>
                   <td>{new Date(item.createTime).toLocaleString()}</td>
                   <td>
-                    <button className="btn btn-primary me-1">수 정</button>
+                    <button
+                      className="btn btn-primary me-1"
+                      onClick={() => editProductRequest(item)}
+                    >
+                      수 정
+                    </button>
                     <button className="btn btn-danger">삭 제</button>
                   </td>
                 </tr>
@@ -72,7 +89,11 @@ const Admin = () => {
         </div>
       </div>
       {/* 모달창 */}
-      <ProductSave ref={saveComponent} onSaved={(p) => saveProductWatcher(p)} />
+      <ProductSave
+        ref={saveComponent}
+        onSaved={(p) => saveProductWatcher(p)}
+        product={selectedProduct}
+      />
     </div>
   );
 };
